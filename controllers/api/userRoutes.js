178dfe46239d58e3d80ba283e76afa7bcf8dac4e
Taylor Shen <shen.taylor@yahoo.com>
2022-06-17 16:20:03 -0700

    fixing login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,52 +2,21 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const bcrypt = require('bcrypt');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.username = userData.username;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
+router.post("/", (req, res) => {
+  User.create(req.body)
+    .then(newUser => {
+      req.session.user = {
+        id:newUser.id,
+        username:newUser.username
+      }
+      res.json(newUser);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ msg: "An error occured", err });
     });
-  } catch (err) {
-    res.status(400).json(err);
-  }
 });
 
-// router.post('/login', async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ where: { username: req.body.username } });
-
-//     if (!userData) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect username or password, please try again' });
-//       return;
-//     }
-
-//     const validPassword = await userData.checkPassword(req.body.password);
-
-//     if (!validPassword) {
-//       res
-//         .status(400)
-//         .json({ message: 'Incorrect username or password, please try again' });
-//       return;
-//     }
-
-//     req.session.save(() => {
-//       req.session.username = userData.username;
-//       req.session.logged_in = true;
-
-//       res.json({ user: userData, message: 'You are now logged in!' });
-//     });
-
-//   } catch (err) {
-//     res.status(400).json(err);
-//   }
-// });
 router.post("/login", (req, res) => {
   User.findOne({
     where: {
